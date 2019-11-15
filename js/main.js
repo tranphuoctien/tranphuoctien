6cab1d7eb66d5d887c94c2b09e0d56a044272882
TienTP <starshinevietnam@gmail.com>
2019-11-15 15:02:47 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿function toggleClass(element) {
+    if (document.getElementById(element).className.match(/(?:^|\s)is-active(?!\S)/)) {
+        document.getElementById(element).className = document.getElementById(element).className.replace(' is-active', '');
+    } else {
+        document.getElementById(element).className += ' is-active';
+    }
+}
